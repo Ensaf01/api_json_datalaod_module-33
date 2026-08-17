35195bfe03ab02ce4549752336b6e61b181eb299
ensaf01 <mohammedensaf402@gmail.com>
2026-08-17 12:22:01 +0600

    json and js diffrenet ty

---
+++ js/json.js
@@ -0,0 +1,20 @@
+// json -> javaScript object Notation
+
+
+
+//JSON.stringify
+//  /* json means it convert js value into json string*/
+
+const user={id:1,name:'Ensaf Hossain'};
+console.log(user); //js
+const stringified=JSON.stringify(user);
+console.log(stringified); //json
+// normal and stringify output almost same but not same
+
+/*  output is
+
+{ id: 1, name: 'ensaf' } //id can be number then normal js create problem thats why json convert all as string for ignore like this problem
+{"id":1,"name":"ensaf"}
+
+*/
+
