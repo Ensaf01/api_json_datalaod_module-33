5954689e203de834eccbd89ed6cfb513e6515e2c
ensaf01 <mohammedensaf402@gmail.com>
2026-08-18 15:51:05 +0600

    add post from json placehoder ,post dynamicaly add in html

---
+++ js/post.js
@@ -0,0 +1,20 @@
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(result => displayPost(result))
+}
+loadPosts();
+function displayPost(posts){
+    console.log(posts);
+    const postContainer=document.getElementById('post-container');
+    for(const post of posts){
+        const DivPost=document.createElement('div');
+        DivPost.classList.add('post')
+        DivPost.innerHTML= `
+        <h3> user-Id: {$post.userId} </h3>
+        <h4> post: {$post.title}</h4>
+        <p> post description: {$post.body}
+        `;
+        postContainer.appendChild(DivPost);
+    }
+}
